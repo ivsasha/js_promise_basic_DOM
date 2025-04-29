8fdbfabb8bead12471af79301ee39806e3c262d7
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-30 00:57:12 +0300

    create promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const message = document.createElement('div');
+const errorMessage = document.createElement('div');
+
+message.className = 'message';
+errorMessage.className = 'error-message';
+errorMessage.className = 'message';
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+promise1
+  .then((messages) => {
+    message.innerHTML = messages;
+  })
+  .catch((error) => {
+    errorMessage.innerHTML = error;
+    logo.appendChild(errorMessage);
+  })
+  .finally(() => {
+    message.innerHTML = 'Promise was resolved!';
+    logo.appendChild(message);
+  });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise 2 resolved'));
+  }, 3000);
+});
+
+promise2
+  .then((messages) => {
+    message.innerHTML = messages;
+  })
+  .catch((error) => {
+    errorMessage.innerHTML = error;
+    logo.appendChild(errorMessage);
+  })
+  .finally(() => {
+    message.innerHTML = 'Promise was resolved!';
+    logo.appendChild(message);
+  });
